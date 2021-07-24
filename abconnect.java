b2f21b0a3bda481e9e2d2e2551151d6ecddafae3
varunkumarswarna <varunkumarswarna@gmail.com>
2021-07-24 00:39:43 +0000

    abcomnnect.java, bcconnect.java

--- abconnect.java
+++
@@ -1,5 +0,0 @@
-i gud morining guys 
-i am here to pratice some time 
-gud to see u guys
-ok sure take care
-bye  
--- bcconnect.java
+++
@@ -1,6 +0,0 @@
-dfdlfjdlfj
-djfdlksfjkdf
-gud moring everyone 
-nice to meet u here
-bye dear
-
